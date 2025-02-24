98a1907b8ee6cca9ad7743ecc7eed1a1e2962cd6
D. Daniel Iramain <danieliramain97@gmail.com>
2025-02-24 20:16:44 -0300

    Class Fundamentals()
    Class Fundamentals() created. Instance is called in st.button

--- stock.py
+++ stock.py
@@ -12,9 +12,7 @@ services = {'Fundamentals': ['overview',
                             'splits',
                             'etf_profile']}
 
-option = st.selectbox(
-    'Select an option',
-     services['Fundamentals'])
+option = st.selectbox('Select an option', services['Fundamentals'])
 
 'You selected: ', option
 
@@ -24,15 +22,62 @@ f'Ticker selected: {st.session_state.ticker}'
 api_key = st.text_input('API key', key='key', type="password")
 f'API key selected: {st.session_state.key}'
 
-def get_data():
-    url = f'https://www.alphavantage.co/query?function={option}&symbol={ticker}&apikey={api_key}'
-    r = requests.get(url)
-    data = r.json()
-    
-    df = pd.DataFrame.from_dict(data, orient='index')
+class Fundamentals():
+    def __init__(self, service: str, ticker: str, api_key: str):    
+        self.__service = service
+        self.__ticker = self.validate_ticker(ticker)
+        self.__api_key = self.validate_api_key(api_key)
 
-    return df
+    @property
+    def service(self):
+        return self.__service
+
+    @property
+    def symbol(self):
+        return self.__ticker
+
+    @property
+    def apikey(self):
+        return self.__api_key
+
+    #@ticker.setter
+    #def ticker(self, new_ticker):
+    #    self.__ticker = self.validate_ticker(new_ticker)
+
+    #@api_key.setter
+    #def api_key(self, new_api_key):
+    #    self.__api_key = self.validate_api_key(new_api_key)
+
+    def validate_ticker(self, ticker: str) -> str:
+        try:
+            new_ticker = str(ticker)
+
+            return new_ticker
+        except ValueError:
+            print('The ticker must be valid')
+
+            return new_ticker
+    
+    def validate_api_key(self, apikey: str) -> str:
+        try:
+            new_api_key = str(api_key)
+
+            return new_api_key
+        except ValueError:
+            print('The apikey must be valid')
+
+            return None
+
+    def get_data(self):
+        url = f'https://www.alphavantage.co/query?function={self.__service}&symbol={self.__ticker}&apikey={self.__api_key}'
+        r = requests.get(url)
+        data = r.json()
+
+        df = pd.DataFrame.from_dict(data, orient='index')
+
+        return df
 
 if st.button('Obtain data'):
-    dataframe = get_data()
-    st.dataframe(dataframe)
+    fundamentals = Fundamentals(option, ticker, api_key)
+    data = fundamentals.get_data()
+    st.dataframe(data)
