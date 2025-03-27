bf2f33b763acf2199f1b02dbc3d1877cce2b0727
D. Daniel Iramain <danieliramain97@gmail.com>
2025-03-27 09:56:23 -0300

    time_series.py markdown

--- stock-info/time_series.py
+++ stock-info/time_series.py
@@ -1,7 +1,15 @@
 import streamlit as st
 
 st.session_state.current_page = "time_series"
-#st.write(f"Current page: {st.session_state.current_page}")
 
-#if st.session_state.current_page == "time_series":
-#    st.write("This content only shows on Page 2")
+st.subheader('Time Series: Provide global equity data in different temporal resolutions', divider=True)
+st.markdown('Time series of :red[**opening, closing, high, and low prices**] along with the volume traded (includes adjusted options) with 20+ years of historical depth.')
+
+st.markdown(':red[**Intraday**]')
+st.markdown(':red[**Daily**]')
+st.markdown(':red[**Daily adjusted**]')
+st.markdown(':red[**Weekly**]')
+st.markdown(':red[**Weekly adjusted**]')
+st.markdown(':red[**Monthly**]')
+st.markdown(':red[**Monthly adjusted**]')
+st.markdown(':red[**Global quote (last price and volume)**]')
